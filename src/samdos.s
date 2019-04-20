a4b91ae7418724a0a78e73eca01e0129f6a7dbfe
Stefan Drissen <stefan.drissen@gmail.com>
2019-04-20 12:10:37 +0200

    remove versions from file names

---
+++ src/samdos.s
@@ -0,0 +1,10 @@
+
+        include "a.s"
+        include "b.s"
+        include "c.s"
+        include "d.s"
+        include "e.s"
+        include "f.s"
+        include "g.s"
+        include "h.s"
+
